efa50bdb8c41e13dca5e68a124ca60d9263f76da
sebastien luquet <sebastien.luquet.2003@anelis.isima.fr>
2009-02-21 16:14:25 +0000

    Encodage qui peut poser pb dans d'autre d'appli Extension des test par DatabaseSwitcher (il reste le pb du fixtures)

--- lib/database_switcher.rb
+++ lib/database_switcher.rb
@@ -9,6 +9,28 @@ def switch_database database
     :database => "#{database}",
     :username => option[1],
     :password => option[2],
+    :encoding => 'utf8',
     :host     => option[0]
   )
+end
+
+module DatabaseSwitcher
+  attr_accessor :database
+
+  def use_database database
+    @database = database
+    include UseRecetteDataBase
+  end
+
+  module UseRecetteDataBase
+    def setup_fixtures
+      @old_database = ActiveRecord::Base.connection.connection_options[3]
+      switch_database(self.class.database)
+      super
+    end
+    def teardown_fixtures
+      super
+      switch_database(@old_database)
+    end
+  end
 end
